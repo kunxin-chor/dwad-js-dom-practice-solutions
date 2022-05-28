3d1ab20dea406b2d165f0bae858c3338181a17de
kunxin-chor <chorkunxin@yahoo.com>
2022-05-28 03:55:39 +0000

    added solution for r2q3

--- r2q3_Transfer_Money/script.js
+++ r2q3_Transfer_Money/script.js
@@ -1,3 +1,69 @@
 // PLACE YOUR CODE HERE
+let walletBalance = 0
+let accountBalance = 0;
+let errorMessage = "";
+refreshWalletBalance();
+refreshAccountBalance();
 
+function refreshWalletBalance() {
+    document.querySelector("#other-wallet").innerHTML = "$" + walletBalance;
+}
 
+function refreshAccountBalance() {
+    document.querySelector("#account").innerHTML = "$" + accountBalance;
+}
+
+function refreshErrorMessage() {
+    console.log("refreshErrorMessage()")
+    let errorDiv = document.querySelector("#error");
+    if (errorMessage != "") {    
+        console.log("Displaying error message")  ;
+        errorDiv.style.display = 'block';
+        errorDiv.innerHTML = errorMessage;
+    } else {
+        errorDiv.style.display = 'none';
+        errorDiv.style.innerHTML = "";
+    }
+}
+
+document.querySelector("#topup10").addEventListener('click', function(){
+    walletBalance += 10;
+    refreshWalletBalance();
+})
+
+document.querySelector("#transfer10ToAccount").addEventListener('click', function(){
+    if (walletBalance > 0) {
+        walletBalance -= 10;
+        accountBalance += 10;
+        refreshAccountBalance();
+        refreshWalletBalance();
+    }
+})
+
+document.querySelector("#transfer10ToWallet").addEventListener('click', function(){
+    if (accountBalance > 0) {
+        walletBalance += 10;
+        accountBalance -= 10;
+        refreshAccountBalance();
+        refreshWalletBalance();
+    }
+
+})
+
+document.querySelector('#spendBtn').addEventListener('click', function(){
+    let spendAmount = Number(document.querySelector('#spend').value);
+
+    // make sure spendAmount is 0, NaN, undefined or null
+    if (spendAmount) {
+        if (walletBalance >= spendAmount) {
+            walletBalance -= spendAmount; // walletBalance = walletBalance - spendAmount
+            refreshWalletBalance();
+            errorMessage = "";
+            refreshErrorMessage();
+        } else {
+            errorMessage = "You don't have enough balance";
+            refreshErrorMessage();
+        }
+     } 
+
+})
