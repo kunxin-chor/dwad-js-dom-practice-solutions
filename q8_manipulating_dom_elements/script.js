356cff807dff0dd9d210fb6d8359ec62009b3009
kunxin-chor <chorkunxin@yahoo.com>
2022-05-28 03:28:27 +0000

    Added solution for q9

--- q8_manipulating_dom_elements/script.js
+++ q8_manipulating_dom_elements/script.js
@@ -1,3 +1,25 @@
 // PLACE YOUR CODE HERE
+let emphasisElements = document.querySelectorAll('.emphasis');
+for (let e of emphasisElements) {
+    e.style.fontFamily = "Verdana"
+}
 
+let finishedElements = document.querySelectorAll('li.finished');
+for (let f of finishedElements) {
+    f.style.textDecoration = "line-through"
+}
 
+// let lowTodos = document.querySelectorAll('ul#low-priority .todo');
+// for (let l of lowTodos) {
+//     l.style.backgroundColor = 'blue'
+// }
+
+let lowPriorityUl = document.querySelector('#low-priority');
+let lowTodos = lowPriorityUl.querySelectorAll('.todo');
+for (let l of lowTodos) {
+    l.style.backgroundColor = 'blue';
+}
+
+document.querySelectorAll('.alert').forEach(function(element){
+    element.style.color = 'red';
+})
--- q9_basic_form_manipulation/script.js
+++ q9_basic_form_manipulation/script.js
@@ -1,13 +1,36 @@
 
 function getFormValues() {
 
-    let email = null;
+    // step 1: select the <input type="email"> element
+    let emailElement = document.querySelector("#email-address");    
+    let email = emailElement.value;
 
-    let model = null;
+    // let model = null;
+    // // get all the radio buttons
+    // let allRadioButtons = document.querySelectorAll(".phone-type");
+    // // use a for loop to determine which one has been checked
+    // for (let rb of allRadioButtons) {
+    //     if (rb.checked) {            
+    //         model = rb.value;
+    //         break;
+    //     }
+    // }
+    let model = document.querySelector('.phone-type:checked').value;
+    
+ 
+    let services = [];
+    // let allCheckboxes = document.querySelectorAll(".services");
+    // for (let eachCheckbox of allCheckboxes) {
+    //     if (eachCheckbox.checked) {
+    //         services.push(eachCheckbox.value)
+    //     }
+    // }
 
-    let services = null;
+     document.querySelectorAll(".services:checked").forEach(function(element){
+        services.push(element.value)
+    })
 
-    let pickup = null;
+    let pickup = document.querySelector("#pick-up").value;
 
     // leave this line alone!
     return [email, model, services, pickup];
