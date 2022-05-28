7771071e8095b207727354fe52e981172b56c889
kunxin-chor <chorkunxin@yahoo.com>
2022-05-28 02:46:49 +0000

    Added ans for qn7

--- q7_selecting_many/script.js
+++ q7_selecting_many/script.js
@@ -1,3 +1,7 @@
 // PLACE YOUR CODE HERE
-
+// use .querySelectorAll() if we want ALL the possible matches
+let todos = document.querySelectorAll("li.todo");
+for (let t of todos) {
+    t.style.backgroundColor = 'orange';
+}
 
