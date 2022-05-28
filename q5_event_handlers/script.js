973ced7a549a8f1db86cd973eb6e698ae33013e5
kunxin-chor <chorkunxin@yahoo.com>
2022-05-28 02:41:47 +0000

    Added qn5 solution

--- q5_event_handlers/script.js
+++ q5_event_handlers/script.js
@@ -1,3 +1,30 @@
-// PLACE YOUR CODE HERE
 
+// E - event
+// I - input
+// P - processing
+// O - output
+
+// step 1
+let buttonElement = document.querySelector('#changeColor');
+
+let numberOfClicks = 0;
+
+// the first argument to addEventListener is the type of event
+buttonElement.addEventListener('click', function(){
+
+
+  let h1Element = document.querySelector('h1');
+  if (numberOfClicks % 3 == 0) {
+    h1Element.style.backgroundColor = 'yellow'
+  } 
+  if (numberOfClicks % 3== 1) {
+      h1Element.style.backgroundColor = 'red';
+  } 
+  if (numberOfClicks % 3 == 2) {
+      h1Element.style.backgroundColor = 'white';
+  }
+  numberOfClicks++;
+  
+  
+})
 
